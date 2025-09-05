efc02523040112c498f9e524bee8ad838c2fb8bc
kovacsrud <kovacs.rud@gmail.com>
2025-09-05 11:59:03 +0200

    Mobilok

---
+++ WpfMobilok/WpfMobilok/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Win32;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfMobilok.Model;
+using WpfMobilok.Views;
+
+namespace WpfMobilok;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public List<Mobil> Mobilok { get; set; } = new List<Mobil>();
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void menuItemKilepes_Click(object sender, RoutedEventArgs e)
+    {
+        Environment.Exit(0);
+        
+    }
+
+    private void menuitemNevjegy_Click(object sender, RoutedEventArgs e)
+    {
+        NevjegyView nevjegyView = new NevjegyView();
+        nevjegyView.ShowDialog();
+    }
+
+    private void menuitemMegnyitas_Click(object sender, RoutedEventArgs e)
+    {
+        OpenFileDialog dialog = new OpenFileDialog();
+        dialog.Filter = ".csv fájlok|*.csv|minden fájl|*.*";
+
+        if (dialog.ShowDialog()==true)
+        {
+            try
+            {
+                Mobilok = new MobilLista(dialog.FileName, ';').Mobilok;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);                
+            }
+        }
+    }
+
+    private void menuitemGyarto_Click(object sender, RoutedEventArgs e)
+    {
+        GyartoView gyarto = new GyartoView(Mobilok);
+        gyarto.ShowDialog();
+    }
+}
---
+++ WpfMobilok/WpfMobilok/Views/GyartoView.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using WpfMobilok.Model;
+
+namespace WpfMobilok.Views
+{
+    /// <summary>
+    /// Interaction logic for GyartoView.xaml
+    /// </summary>
+    public partial class GyartoView : Window
+    {
+        public List<Mobil> Mobilok { get; set; } = new List<Mobil>();
+        public GyartoView(List<Mobil> mobilok)
+        {
+            InitializeComponent();
+            Mobilok = mobilok;
+            datagridMobilok.ItemsSource = mobilok;
+            var gyartoLista=Mobilok.OrderBy(x=>x.Gyarto).Select(x=>x.Gyarto).Distinct().ToList();
+            comboGyarto.ItemsSource = gyartoLista;
+        }
+
+        private void comboGyarto_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var kivalasztottGyarto=comboGyarto.SelectedItem as string;
+
+            var result = Mobilok.FindAll(x => x.Gyarto == kivalasztottGyarto);
+
+            datagridMobilok.ItemsSource= result;
+        }
+
+        private void buttonVissza_Click(object sender, RoutedEventArgs e)
+        {
+            datagridMobilok.ItemsSource = Mobilok;
+        }
+    }
+}
