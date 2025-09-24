7d7af3e4d363ed20c2dcf2ea01d03947709821d7
kovacsrud <kovacs.rud@gmail.com>
2025-09-24 14:39:17 +0200

    Mvvm minta megvalósítás

---
+++ WpfMvvmDronMobil/WpfMvvmDronMobil/Config.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfMvvmDronMobil
+{
+    public static class Config
+    {
+        public static char MobilHatarolo { get; set; } = ';';
+        public static char DronHatarolo { get; set; } = ';';
+        public static string MobilFajl { get; set; } = "mobil_adatok_win_pv.csv";
+        public static string DronFajl { get; set; } = "dronok.csv";
+        public static int DronStart { get; set; } = 1;
+        public static int MobilStart { get; set; } = 1;
+
+    }
+}
---
+++ WpfMvvmDronMobil/WpfMvvmDronMobil/Mvvm/ViewModel/MobilDronViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using WpfMvvmDronMobil.Mvvm.Model;
+
+namespace WpfMvvmDronMobil.Mvvm.ViewModel
+{
+    public class MobilDronViewModel
+    {
+        public List<Dron> Dronok { get; set; }=new List<Dron>();
+        public List<Mobil> Mobilok { get; set; } = new List<Mobil>();
+
+        public Dron SelectedDron { get; set; } = new Dron();
+        public Mobil SelectedMobil { get; set; } = new Mobil();
+
+        public MobilDronViewModel()
+        {
+            try
+            {
+                var dronAdatok = File.ReadAllLines(Config.DronFajl, Encoding.Default);
+                for (int i = Config.DronStart; i < dronAdatok.Length; i++)
+                {
+                    Dronok.Add(new Dron(dronAdatok[i], Config.DronHatarolo));
+                }
+
+                var mobilAdatok=File.ReadAllLines(Config.MobilFajl,Encoding.Default);
+                for (int i = Config.MobilStart; i < mobilAdatok.Length; i++)
+                {
+                    Mobilok.Add(new Mobil(mobilAdatok[i], Config.MobilHatarolo));
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);                
+            }   
+        }
+    }
+}
