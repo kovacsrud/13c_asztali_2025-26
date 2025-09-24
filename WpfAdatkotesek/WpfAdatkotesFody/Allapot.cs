09c4ee5deb470d48e9a56f3d57855147c3129be8
kovacsrud <kovacs.rud@gmail.com>
2025-09-24 13:29:27 +0200

    Adatkötések

---
+++ WpfAdatkotesek/WpfAdatkotesFody/Allapot.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PropertyChanged;
+
+namespace WpfAdatkotesFody
+{
+    [AddINotifyPropertyChangedInterface]
+    public class Allapot
+    {
+        public int Ertek { get; set; }
+
+        public Allapot(int ertek)
+        {
+            Ertek= ertek;    
+        }
+    }
+}
---
+++ WpfAdatkotesek/WpfAdatkotesek/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfAdatkotesek
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void textboxAllapot_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            try
+            {
+                sliderAllapot.Value = Convert.ToDouble(textboxAllapot.Text);
+                progressbarAllapot.Value = Convert.ToDouble(textboxAllapot.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                
+            }
+            
+        }
+
+        private void sliderAllapot_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            textboxAllapot.Text = sliderAllapot.Value.ToString();
+            progressbarAllapot.Value = sliderAllapot.Value;
+        }
+    }
+}
