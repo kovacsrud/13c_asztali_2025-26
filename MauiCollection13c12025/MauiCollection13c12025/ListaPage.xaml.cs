bdd7204db99ed9f016ba0dbbb22ace27f1a0708c
kovacsrud <kovacs.rud@gmail.com>
2025-11-05 14:38:55 +0100

    Kész.

--- MauiCollection13c12025/MauiCollection13c12025/ListaPage.xaml.cs
+++ MauiCollection13c12025/MauiCollection13c12025/ListaPage.xaml.cs
@@ -1,9 +1,29 @@
+using System.Collections.ObjectModel;
+
 namespace MauiCollection13c12025;
 
 public partial class ListaPage : ContentPage
 {
-	public ListaPage()
+    ObservableCollection<Hegy> Hegyek = new ObservableCollection<Hegy>();
+    public ListaPage()
 	{
 		InitializeComponent();
 	}
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        await LoadMauiAsset();
+        BindingContext = Hegyek;
+    }
+
+    async Task LoadMauiAsset()
+    {
+        using var stream = await FileSystem.OpenAppPackageFileAsync("hegyekMo.txt");
+        using var reader = new StreamReader(stream);
+        reader.ReadLine();
+        while (!reader.EndOfStream)
+        {
+            Hegyek.Add(new Hegy(reader.ReadLine()));
+        }
+    }
 }
