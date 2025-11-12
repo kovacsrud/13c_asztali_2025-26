5ad96357a1356125f4e04714e83da3bdb6f07174
kovacsrud <kovacs.rud@gmail.com>
2025-11-12 11:49:23 +0100

    Nézetek létrehozása

---
+++ MauiRandomuser13c12025/MauiRandomuser13c12025/Views/DetailPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace MauiRandomuser13c12025.Views;
+
+public partial class DetailPage : ContentPage
+{
+	public DetailPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MauiRandomuser13c12025/MauiRandomuser13c12025/Views/ListPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace MauiRandomuser13c12025.Views;
+
+public partial class ListPage : ContentPage
+{
+	public ListPage()
+	{
+		InitializeComponent();
+	}
+}
