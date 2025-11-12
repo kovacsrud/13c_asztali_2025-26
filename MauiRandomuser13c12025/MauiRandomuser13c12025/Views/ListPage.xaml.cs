3650757b86f80f30c01cf0bdb0a61c3383f11a15
kovacsrud <kovacs.rud@gmail.com>
2025-11-12 13:40:40 +0100

    Osztály generálás

--- MauiRandomuser13c12025/MauiRandomuser13c12025/Views/ListPage.xaml.cs
+++ MauiRandomuser13c12025/MauiRandomuser13c12025/Views/ListPage.xaml.cs
@@ -2,8 +2,14 @@ namespace MauiRandomuser13c12025.Views;
 
 public partial class ListPage : ContentPage
 {
+
 	public ListPage()
 	{
 		InitializeComponent();
 	}
+
+    private void buttonLetolt_Clicked(object sender, EventArgs e)
+    {
+
+    }
 }
