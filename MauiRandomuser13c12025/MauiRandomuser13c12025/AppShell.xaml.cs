11144d74f2fc0639df491a2a6f72eb4977f90aef
kovacsrud <kovacs.rud@gmail.com>
2025-11-12 11:40:34 +0100

    Start, fontok hozzáadása

---
+++ MauiRandomuser13c12025/MauiRandomuser13c12025/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiRandomuser13c12025
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
