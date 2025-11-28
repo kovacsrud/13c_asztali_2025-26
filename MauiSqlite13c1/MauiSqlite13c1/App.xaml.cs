a51713b6fe766e18703c7419f51630e0c9286edd
kovacsrud <kovacs.rud@gmail.com>
2025-11-28 10:12:22 +0100

    Jegyzet app, modell létrehozás

---
+++ MauiSqlite13c1/MauiSqlite13c1/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiSqlite13c1
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MauiSqlite13c1/MauiSqlite13c1/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MauiSqlite13c1
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
