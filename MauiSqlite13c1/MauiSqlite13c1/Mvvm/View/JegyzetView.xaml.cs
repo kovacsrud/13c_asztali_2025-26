30d002257984489c5ff8936528b31e2e156621ca
kovacsrud <kovacs.rud@gmail.com>
2025-11-28 10:44:31 +0100

    Felület létrehozása

---
+++ MauiSqlite13c1/MauiSqlite13c1/Mvvm/View/JegyzetView.xaml.cs
@@ -0,0 +1,14 @@
+namespace MauiSqlite13c1.Mvvm.View;
+
+public partial class JegyzetView : ContentPage
+{
+	public JegyzetView()
+	{
+		InitializeComponent();
+	}
+
+    private void buttonUj_Clicked(object sender, EventArgs e)
+    {
+
+    }
+}
