19db067f45c1917865a052f3698764f9b90ff9ca
kovacsrud <kovacs.rud@gmail.com>
2025-11-28 11:40:50 +0100

    Adatbázis funkciók megvalósítása

--- MauiSqlite13c1/MauiSqlite13c1/App.xaml.cs
+++ MauiSqlite13c1/MauiSqlite13c1/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace MauiSqlite13c1
+﻿using MauiSqlite13c1.Mvvm.View;
+
+namespace MauiSqlite13c1
 {
     public partial class App : Application
     {
@@ -6,7 +8,7 @@
         {
             InitializeComponent();
 
-            MainPage = new AppShell();
+            MainPage = new JegyzetView();
         }
     }
 }
--- MauiSqlite13c1/MauiSqlite13c1/Mvvm/View/JegyzetView.xaml.cs
+++ MauiSqlite13c1/MauiSqlite13c1/Mvvm/View/JegyzetView.xaml.cs
@@ -1,14 +1,81 @@
+using MauiSqlite13c1.Mvvm.Model;
+using PropertyChanged;
+using SQLite;
+
 namespace MauiSqlite13c1.Mvvm.View;
 
+[AddINotifyPropertyChangedInterface]
 public partial class JegyzetView : ContentPage
 {
-	public JegyzetView()
+    static string dbname = "jegyzetek.db";
+    public const SQLiteOpenFlags Flags = SQLiteOpenFlags.ReadWrite | SQLiteOpenFlags.Create | SQLiteOpenFlags.SharedCache;
+
+    string dbpath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory,dbname);
+
+    SQLiteConnection connection;
+
+    public List<Jegyzet> Jegyzetek { get; set; }= new List<Jegyzet>();
+    public Jegyzet AktualisJegyzet { get; set; }
+
+    public JegyzetView()
 	{
 		InitializeComponent();
+        connection = new SQLiteConnection(dbpath, Flags);
+        connection.CreateTable<Jegyzet>();
+        GetJegyzetek();
+        BindingContext = this;
+
 	}
 
     private void buttonUj_Clicked(object sender, EventArgs e)
     {
+        try
+        {
+            var jegyzet = new Jegyzet { Cim = entryCim.Text, Szoveg = entrySzoveg.Text };
+            connection.Insert(jegyzet);
+            GetJegyzetek();
 
+        }
+        catch (Exception ex)
+        {
+            DisplayAlert("Hiba", ex.Message, "Ok");            
+        }
+    }
+
+  
+
+    private void buttonModosit_Clicked(object sender, EventArgs e)
+    {
+        try
+        {
+            connection.Update(AktualisJegyzet);
+            GetJegyzetek();
+            collectionJegyzetek.SelectedItem= null;
+        }
+        catch (Exception ex)
+        {
+
+            DisplayAlert("Hiba", ex.Message, "Ok");
+        }
+    }
+
+    private void buttonTorol_Clicked(object sender, EventArgs e)
+    {
+        try
+        {
+            connection.Delete(AktualisJegyzet);
+            GetJegyzetek();
+            collectionJegyzetek.SelectedItem = null;
+        }
+        catch (Exception ex)
+        {
+
+            DisplayAlert("Hiba", ex.Message, "Ok");
+        }
+    }
+
+    private void GetJegyzetek()
+    {
+        Jegyzetek = connection.Table<Jegyzet>().ToList();
     }
 }
