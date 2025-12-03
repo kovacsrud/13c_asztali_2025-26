6a70353b68fae708fc7249f11c7c7e899f5071b4
kovacsrud <kovacs.rud@gmail.com>
2025-12-03 13:07:25 +0100

    Jegyzet osztály létrehozása

---
+++ MauiJegyzet13c12025/MauiJegyzet13c12025/mvvm/model/Jegyzet.cs
@@ -0,0 +1,22 @@
+﻿using MauiJegyzet13c12025.interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+using SQLite;
+
+namespace MauiJegyzet13c12025.mvvm.model
+{
+    [System.ComponentModel.DataAnnotations.Schema.Table("jegyzetek")]
+    public class Jegyzet:TableData
+    {
+        [NotNull]
+        public string Cim { get; set; }
+        [NotNull]
+        public string Szoveg { get; set; }
+        [NotNull]
+        public DateTime Datum { get; set; } = DateTime.Now;
+    }
+}
