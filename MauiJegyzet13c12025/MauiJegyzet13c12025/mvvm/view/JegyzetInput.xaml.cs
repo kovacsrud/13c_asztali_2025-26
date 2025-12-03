ffdf7f367baf3a875c2982d060b4345a2b6c5e26
kovacsrud <kovacs.rud@gmail.com>
2025-12-03 14:17:06 +0100

    JegyzetInput XAML létrehozása

---
+++ MauiJegyzet13c12025/MauiJegyzet13c12025/mvvm/view/JegyzetInput.xaml.cs
@@ -0,0 +1,14 @@
+namespace MauiJegyzet13c12025.mvvm.view;
+
+public partial class JegyzetInput : ContentPage
+{
+	public JegyzetInput()
+	{
+		InitializeComponent();
+	}
+
+    private void buttonInput_Clicked(object sender, EventArgs e)
+    {
+
+    }
+}
