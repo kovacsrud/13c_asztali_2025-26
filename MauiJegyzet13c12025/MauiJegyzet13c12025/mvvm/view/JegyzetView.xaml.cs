41f5d7200c22173711fbf4d37d25872d9507efd1
kovacsrud <kovacs.rud@gmail.com>
2025-12-03 14:02:20 +0100

    JegyzetView elkészítése

--- MauiJegyzet13c12025/MauiJegyzet13c12025/mvvm/view/JegyzetView.xaml.cs
+++ MauiJegyzet13c12025/MauiJegyzet13c12025/mvvm/view/JegyzetView.xaml.cs
@@ -1,3 +1,5 @@
+using MauiJegyzet13c12025.mvvm.viewmodel;
+
 namespace MauiJegyzet13c12025.mvvm.view;
 
 public partial class JegyzetView : ContentPage
@@ -5,5 +7,21 @@ public partial class JegyzetView : ContentPage
 	public JegyzetView()
 	{
 		InitializeComponent();
+        BindingContext = new JegyzetViewModel();
 	}
+
+    private void buttonUj_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void buttonModosit_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void buttonTorol_Clicked(object sender, EventArgs e)
+    {
+
+    }
 }
