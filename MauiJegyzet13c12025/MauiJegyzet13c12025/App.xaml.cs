c1e39f40c97ac830591968fba03ccdf770ba582f
kovacsrud <kovacs.rud@gmail.com>
2025-12-03 13:25:16 +0100

    Nézet elkészítése, repo beállítása

--- MauiJegyzet13c12025/MauiJegyzet13c12025/App.xaml.cs
+++ MauiJegyzet13c12025/MauiJegyzet13c12025/App.xaml.cs
@@ -1,12 +1,17 @@
-﻿namespace MauiJegyzet13c12025
+﻿using MauiJegyzet13c12025.mvvm.model;
+using MauiJegyzet13c12025.mvvm.view;
+using MauiJegyzet13c12025.repository;
+
+namespace MauiJegyzet13c12025
 {
     public partial class App : Application
     {
-        public App()
+        public static BaseRepository<Jegyzet> JegyzetRepo { get; private set; }
+        public App(BaseRepository<Jegyzet> repo)
         {
             InitializeComponent();
-
-            MainPage = new AppShell();
+            JegyzetRepo = repo;
+            MainPage = new NavigationPage(new JegyzetView());
         }
     }
 }
---
+++ MauiJegyzet13c12025/MauiJegyzet13c12025/mvvm/view/JegyzetView.xaml.cs
@@ -0,0 +1,9 @@
+namespace MauiJegyzet13c12025.mvvm.view;
+
+public partial class JegyzetView : ContentPage
+{
+	public JegyzetView()
+	{
+		InitializeComponent();
+	}
+}
