345a8eb923e50456d49dce14d43de60e655a0a56
kovacsrud <kovacs.rud@gmail.com>
2026-01-14 14:24:29 +0100

    Felület kialakítása

--- WpfKartyak/WpfKartyak/Kartyak.Designer.cs
+++ WpfKartyak/WpfKartyak/Kartyak.Designer.cs
@@ -291,16 +291,6 @@ namespace WpfKartyak {
         }
         
         /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap _6_4_6_1 {
-            get {
-                object obj = ResourceManager.GetObject("6_4_6_1", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
         ///   Looks up a localized resource of type System.Byte[].
         /// </summary>
         internal static byte[] _7_1_7_1 {
