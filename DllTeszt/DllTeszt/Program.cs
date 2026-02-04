cc872e709b39bd20cdeecd50c41b8f81be52070e
kovacsrud <kovacs.rud@gmail.com>
2026-02-04 11:51:55 +0100

    Dll teszt start

---
+++ DllTeszt/DllTeszt/Program.cs
@@ -0,0 +1,21 @@
+﻿using KRHash;
+
+namespace DllTeszt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dll használata");
+            CreateHash hash = new CreateHash();
+
+            var md5Hash = hash.MakeHash(HashType.MD5, "valami");
+            var md5HashUpper = hash.MakeHash(HashType.MD5, "valami",true);
+
+            Console.WriteLine(md5Hash);
+            Console.WriteLine(md5HashUpper);
+
+
+        }
+    }
+}
