245667830821ae371f42e035edc4749f9295a76e
kovacsrud <kovacs.rud@gmail.com>
2026-02-25 12:43:31 +0100

    Versenyzői adatok lekérdezése, megjelenítése

--- Maratonvalto/MaratonvaltoGUI/MainWindow.xaml.cs
+++ Maratonvalto/MaratonvaltoGUI/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using Maratonvalto;
+using Microsoft.Win32;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -16,13 +18,46 @@ namespace MaratonvaltoGUI
     /// </summary>
     public partial class MainWindow : Window
     {
+        public List<Eredmeny> Eredmenyek { get; set; }=new List<Eredmeny>();
+        public Eredmeny KivalasztottEredmeny { get; set; }
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = this;
         }
 
         private void menuitemMegnyitas_Click(object sender, RoutedEventArgs e)
         {
+            OpenFileDialog dialog=new OpenFileDialog();
+
+            if (dialog.ShowDialog()==true)
+            {
+                try
+                {
+                    Eredmenyek = LoadData.LoadFromCsv(dialog.FileName);
+                    listboxFutok.ItemsSource=Eredmenyek.OrderBy(x=>x.Versenyzo.Fnev).Select(x=>x.Versenyzo.Fnev).Distinct().ToList();   
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);                    
+                }
+            }
+         }
+
+        private void listboxFutok_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var kivalasztottNev = listboxFutok.SelectedItem as string;
+            var kivalasztott=Eredmenyek.Find(x=>x.Versenyzo.Fnev==kivalasztottNev);
+
+            if (kivalasztott != null)
+            {
+                textboxNev.Text = kivalasztott.Versenyzo.Fnev;
+                textboxRajtszam.Text = kivalasztott.Versenyzo.Fid.ToString();
+                textboxSzulEvHo.Text = $"{kivalasztott.Versenyzo.Szulev} - {kivalasztott.Versenyzo.Szulho}";
+                textboxKor.Text=kivalasztott.Kor.ToString();
+                textboxIdo.Text=kivalasztott.Ido.ToString();
+                textboxCsapatSzam.Text=kivalasztott.Versenyzo.Csapat.ToString();
+            }
 
         }
     }
