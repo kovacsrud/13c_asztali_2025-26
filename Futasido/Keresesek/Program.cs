48539b3ae16afb3ec13790461b67947e81f3d0f5
kovacsrud <kovacs.rud@gmail.com>
2026-03-18 13:04:15 +0100

    Futásidő komplexitás

---
+++ Futasido/Keresesek/Program.cs
@@ -0,0 +1,69 @@
+﻿using System.Diagnostics;
+
+namespace Keresesek
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 10000;
+            Random rand= new Random();
+            Stopwatch stopper = new Stopwatch();
+            int[] szamok=new int[n];
+
+            for(int i=0; i<szamok.Length; i++)
+            {
+                szamok[i] = rand.Next(0, 1000 + 1);
+            }
+            int keresett= 131;
+            stopper.Start();
+            Console.WriteLine($"A keresett elem indexe:{LinearisKereses(keresett, szamok)}");
+            stopper.Stop();
+            Console.WriteLine($"Lineáris keresés végrehajtási idő:{stopper.ElapsedTicks}");
+            stopper.Reset();
+            stopper.Start();
+            Console.WriteLine($"A keresett elem indexe:{BinarisKereses(keresett,szamok)}");
+            stopper.Stop();
+            Console.WriteLine($"Bináris keresés végrehajtási idő:{stopper.ElapsedTicks}");
+
+        }
+
+        private static int BinarisKereses(int keresett, int[] tomb)
+        {
+            Array.Sort(tomb);
+            int bal = 0;
+            int jobb = tomb.Length - 1;
+
+            while (bal<=jobb)
+            {
+                int kozep=bal+(jobb-bal)/2;
+
+                if (tomb[kozep] == keresett)
+                {
+                    return kozep;
+                }
+                else if (tomb[kozep] < keresett)
+                {
+                    bal = kozep + 1;
+                }
+                else {
+                    jobb = kozep - 1;
+                }
+            }
+            return -1;
+        }
+
+        private static int LinearisKereses(int keresett, int[] tomb)
+        {
+            for (int i = 0; i < tomb.Length; i++) {
+
+                if (tomb[i] == keresett) { 
+
+                    return i;
+                }
+            
+            }
+            return -1;
+        }
+    }
+}
