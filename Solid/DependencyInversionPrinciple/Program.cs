bd5c61ac07c17c3d5a2ac3222aea4cbbc7c42112
kovacsrud <kovacs.rud@gmail.com>
2026-03-25 13:48:00 +0100

    SOLID kész

---
+++ Solid/DependencyInversionPrinciple/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace DependencyInversionPrinciple
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("A függőségek megfordításának elve");
+
+            //A magas szintű modulok ne függjenek az alacsony szintű moduloktól. Mindkettő absztrakcióktól függjön
+            
+            var rosszUserService=new RosszUserService();
+
+            var userService = new JoUserService(new OracleDB());
+            userService.UserMentes("Béla");
+
+        }
+    }
+}
---
+++ Solid/DependencyInversionPrinciple/RosszUserService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInversionPrinciple
+{
+    public class RosszUserService
+    {
+        //függőség
+        private MysqlDB mysql=new MysqlDB();
+
+        public void UserMentes(string user)
+        {
+            mysql.DbMentes(user);
+        }
+
+    }
+}
